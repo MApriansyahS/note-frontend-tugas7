e92d1bf7c15d3121bec6ce189a3e6b632a8d7315
MuhammadApriansyahSaputra <123220142@student.upnyk.ac.id>
2025-05-28 13:42:37 +0700

    fix

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://notes-backend-559917148272.us-central1.run.app/", 
+  baseURL: "https://notes-backend-559917148272.us-central1.run.app", 
   withCredentials: true, 
 });
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://notes-backend-559917148272.us-central1.run.app/";
+export const BASE_URL = "https://notes-backend-559917148272.us-central1.run.app";
